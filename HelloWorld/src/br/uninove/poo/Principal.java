f54db6d58a9b072dfa0e206de761ada1cc24b51b
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-02-24 22:03:55 -0300

    Projeto HelloWorld

---
+++ HelloWorld/src/br/uninove/poo/Principal.java
@@ -0,0 +1,10 @@
+package br.uninove.poo;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        //paramos aqui.. continuremos aula que vem....
+        
+    }
+    
+}
